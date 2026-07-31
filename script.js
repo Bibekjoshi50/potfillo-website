3697a5dc011c4f1c4f24a0ae5a8b8d56e2164ace
Your Name <bibekjoshi118@gmail.com>
2026-07-31 20:57:56 +0545

    new

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+
+const menu = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menu.addEventListener("click", function(){
+
+    navLinks.classList.toggle("active")
+
+})
+
+</script>
